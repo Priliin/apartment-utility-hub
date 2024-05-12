66e07d2c0b867417b38b85280b117d2d407da2f8
Renar Riitsaar <renar.riitsaar@gmail.com>
2024-05-12 13:52:22 +0300

    front and back

---
+++ src/main/java/com/utilityhub/apartmentutilityhub/service/ApartmentService.java
@@ -0,0 +1,11 @@
+package com.utilityhub.apartmentutilityhub.service;
+
+import com.utilityhub.apartmentutilityhub.dto.ApartmentDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface ApartmentService {
+    List<ApartmentDTO> findAllApartments();
+}
