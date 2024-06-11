507be97016f8457473c023283ba32b19b9c91054
Liina <jaliin08@hotmail.com>
2024-06-11 16:21:49 +0300

    Main page initial setup

--- src/main/java/com/utilityhub/apartmentutilityhub/service/ApartmentService.java
+++ src/main/java/com/utilityhub/apartmentutilityhub/service/ApartmentService.java
@@ -1,8 +1,6 @@
 package com.utilityhub.apartmentutilityhub.service;
 
 import com.utilityhub.apartmentutilityhub.dto.ApartmentDTO;
-import com.utilityhub.apartmentutilityhub.dto.EventDTO;
-import com.utilityhub.apartmentutilityhub.dto.UserDTO;
 import com.utilityhub.apartmentutilityhub.model.Apartment;
 import org.springframework.stereotype.Service;
 
